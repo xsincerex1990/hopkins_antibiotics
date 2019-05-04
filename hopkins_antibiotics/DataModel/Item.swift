e6053f78c8f71c72d1a89f64384d3c3159597185
xsincerex1990 <xsincerex1990@gmail.com>
2019-05-04 16:55:32 -0400

    set up core data

---
+++ hopkins_antibiotics/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  hopkins_antibiotics
+//
+//  Created by Joel alexis on 5/4/19.
+//  Copyright © 2019 Joel alexis. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var done = false
+}
